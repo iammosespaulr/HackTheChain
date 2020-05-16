fdf82f4d0d42ed188c822a763c4b807c74dbfaa0
Moses Paul R <iammosespaulr@gmail.com>
2020-05-16 17:53:48 +0530

    init react app

---
+++ to-be-named/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This works so far!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/iammosespaulr/HackTheChain"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          The Source
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

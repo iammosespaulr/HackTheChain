9e928ecb66a6917f58a5b938e0c81d167e5865c5
dquach217 <dq6180@rit.edu>
2020-05-17 00:48:52 -0400

    authentication works

--- to-be-named/src/users/controllers/auth.js
+++ to-be-named/src/users/controllers/auth.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const { User } = require('../models/user');
+User = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
@@ -12,22 +12,25 @@ exports.auth = async function(req, res) {
 		return res.status(400).send(error.details[0].message);
 	}
 
-    //  Now find the user by their email address
-    return new Promise((resolve, reject) => {
+	//  Now find the user by their email address
 	let user = await User.findOne({ email: req.body.email });
 	if (!user) {
 		return res.status(400).send('Incorrect email or password.');
 	}
-
 	// Then validate the Credentials in MongoDB match
 	// those provided in the request
-	const validPassword = await bcrypt.compare(req.body.password, user.password);
-	if (!validPassword) {
-		return res.status(400).send('Incorrect email or password.');
-	}
-	res.json({
-		message: 'success',
-		data: true
+	const validPassword = await bcrypt.compare(req.body.password, user.password).then(function(result) {
+		if (!result) {
+			res.json({
+				message: 'error',
+				data: result
+			});
+		} else {
+			res.json({
+				message: 'success',
+				data: true
+			});
+		}
 	});
 };
 
--- to-be-named/src/users/controllers/controller.js
+++ to-be-named/src/users/controllers/controller.js
@@ -20,35 +20,34 @@ exports.index = function(req, res) {
 };
 // Handle create user actions
 exports.new = async function(req, res) {
-	return new Promise((resolve, reject) => {
-		User.findOne({ email: req.body.email }).exec((err, doc) => {
-			if (err) return PromiseRejectionEvent(err);
-			if (doc) return PromiseRejectionEvent(new Error('That user already exists!'));
-			var newUser = new User({
-				first_name: req.body.first_name,
-				last_name: req.body.last_name,
-				phone: req.body.phone,
-				email: req.body.email,
-				password: req.body.password,
-				account_id: req.body.account_id,
-				private_key: req.body.private_key
-			});
-			const salt = bcrypt.genSalt(10);
-			newUser.password = bcrypt.hash(newUser.password, salt);
-			newUser.account_id = bcrypt.hash(newUser.account_id, salt);
-			newUser.private_key = bcrypt.hash(newUser.private_key, salt);
-			// save the user and check for errors
-			newUser.save(function(err) {
-				// Check for validation error
-				if (err) res.json(err);
-				else
-					res.json({
-						message: 'New user created!',
-						data: newUser
-					});
-			});
+	let check = await User.find({ email: req.body.email });
+	if (check[0] != null) {
+		return res.status(400).send('That user already exists!');
+	} else {
+		var user = new User({
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			phone: req.body.phone,
+			email: req.body.email,
+			password: req.body.password,
+			account_id: req.body.account_id,
+			private_key: req.body.private_key
 		});
-	});
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(user.password, salt);
+		user.account_id = await bcrypt.hash(user.account_id, salt);
+		user.private_key = await bcrypt.hash(user.private_key, salt);
+		// save the user and check for errors
+		user.save(function(err) {
+			// Check for validation error
+			if (err) res.json(err);
+			else
+				res.json({
+					message: 'New user created!',
+					data: user
+				});
+		});
+	}
 };
 // Handle view user info
 exports.view = function(req, res) {

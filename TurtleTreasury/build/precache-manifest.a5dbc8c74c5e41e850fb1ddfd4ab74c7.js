4885668621f5eb65fc3f85e8c9f4005c69545a5e
Moses Paul R <iammosespaulr@gmail.com>
2020-05-18 23:56:07 +0530

    will this work ?

---
+++ TurtleTreasury/build/precache-manifest.a5dbc8c74c5e41e850fb1ddfd4ab74c7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "830ef2c38cb537e405f47a5ea20922c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2fe0c5e57bce97237c6",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "9bf5555c85220194af5a",
+    "url": "/static/css/main.6c13ef9c.chunk.css"
+  },
+  {
+    "revision": "f2fe0c5e57bce97237c6",
+    "url": "/static/js/2.dd3e1bf8.chunk.js"
+  },
+  {
+    "revision": "883683d50f50ab55de789354482552fa",
+    "url": "/static/js/2.dd3e1bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf5555c85220194af5a",
+    "url": "/static/js/main.2b3df8f5.chunk.js"
+  },
+  {
+    "revision": "1a28a112d3dd9241690f",
+    "url": "/static/js/runtime-main.6fdda87d.js"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "/static/media/AvenirLTStd-Book.ecb0c2ae.otf"
+  },
+  {
+    "revision": "bdfc65f8ad3c668217efdceb3bfda759",
+    "url": "/static/media/Florida_Box_Turtle_Digon3_re-edited.bdfc65f8.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "1fb1b857cf124047e90af9a87d35a8bc",
+    "url": "/static/media/artur-tumasjan-qLzWvcQq-V8-unsplash.1fb1b857.jpg"
+  },
+  {
+    "revision": "f6a87c024c713975d84720571be39dc3",
+    "url": "/static/media/austin-pacheco-FtL07GM9Q7Y-unsplash.f6a87c02.jpg"
+  },
+  {
+    "revision": "76ae905b757092465b7be871e529b1a7",
+    "url": "/static/media/ben-white-4K2lIP0zc_k-unsplash.76ae905b.jpg"
+  },
+  {
+    "revision": "bb0ee073b04566c90db49ed5d6a3dc32",
+    "url": "/static/media/ewscripps.brightspotcdn.bb0ee073.png"
+  },
+  {
+    "revision": "6d73dc212a700af0b951743cba2e4f58",
+    "url": "/static/media/facts-turtles.6d73dc21.jpg"
+  },
+  {
+    "revision": "4d5b70ff251eb1ee949340621d0743ef",
+    "url": "/static/media/kevin-mccutcheon-APDMfLHZiRA-unsplash.4d5b70ff.jpg"
+  },
+  {
+    "revision": "1209fb5dfbbb29d8849ac36ce33f6d59",
+    "url": "/static/media/leaf-in-coins.1209fb5d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "84454181155352af46b8c5963aea6561",
+    "url": "/static/media/logo192.84454181.png"
+  }
+]);
